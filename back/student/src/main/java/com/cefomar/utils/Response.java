548878a643b71e5ab6bb7799d51809d7c5433251
root3202 <root.illivan@proton.me>
2024-05-01 18:13:24 +0300

    update response

--- back/student/src/main/java/com/cefomar/utils/Response.java
+++ back/student/src/main/java/com/cefomar/utils/Response.java
@@ -6,6 +6,15 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Builder
 public class Response {
     private LocalDateTime timeStamp;
     private HttpStatus status;
