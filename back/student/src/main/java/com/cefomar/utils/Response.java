8d5d8fe248e9bd14646abbbe2560fd94889f2022
root3202 <root.illivan@proton.me>
2024-05-01 12:09:49 +0300

    add response class

--- back/student/src/main/java/com/cefomar/utils/Response.java
+++ back/student/src/main/java/com/cefomar/utils/Response.java
@@ -1,5 +1,18 @@
 package com.cefomar.utils;
 
+import java.net.URI;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+
 public class Response {
+    private Instant timeStamp;
+    private HttpStatus status;
+    private int statusCode;
+    private Map<?, ?> data;
+    private URI locations;
+    private String message;
 
 }
