9744a9fd3775924d78c9de3ccf0c029d71735b5a
root3202 <root.illivan@proton.me>
2024-05-01 12:14:41 +0300

    update response

--- back/student/src/main/java/com/cefomar/utils/Response.java
+++ back/student/src/main/java/com/cefomar/utils/Response.java
@@ -1,14 +1,13 @@
 package com.cefomar.utils;
 
 import java.net.URI;
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 
 public class Response {
-    private Instant timeStamp;
+    private LocalDateTime timeStamp;
     private HttpStatus status;
     private int statusCode;
     private Map<?, ?> data;
