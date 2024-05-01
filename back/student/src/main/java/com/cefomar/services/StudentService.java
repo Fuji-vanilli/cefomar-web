b1a7c828562695e0d473cb9d0dfaed29fe54f036
root3202 <root.illivan@proton.me>
2024-05-01 16:41:37 +0300

    method of student service

--- back/student/src/main/java/com/cefomar/services/StudentService.java
+++ back/student/src/main/java/com/cefomar/services/StudentService.java
@@ -1,5 +1,12 @@
 package com.cefomar.services;
 
-public class StudentService {
+import com.cefomar.dto.StudentRequest;
+import com.cefomar.utils.Response;
 
+public interface StudentService {
+    Response add(StudentRequest request);
+    Response update(StudentRequest request);
+    Response get(String id);
+    Response all();
+    Response delete(String id);
 }
