d39112653736fb68210a1a73b4b50d656e432945
Fuji-vanilli <ijufillivan@gmail.com>
2024-05-03 12:07:39 +0300

    update gateway

--- back/gateway-service/src/main/java/com/cefomar/gatewayservice/GatewayServiceApplication.java
+++ back/gateway-service/src/main/java/com/cefomar/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,24 @@ package com.cefomar.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator routeLocator(
+			ReactiveDiscoveryClient rdc,
+			DiscoveryLocatorProperties dlp
+	) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
 	}
 
 }
