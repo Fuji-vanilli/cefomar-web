9460d31732c473020eb2521253a9e2420ce788a1
Fuji-vanilli <ijufillivan@gmail.com>
2024-05-04 12:43:15 +0300

    formation service

---
+++ back/formation-service/src/main/java/com/cefomar/formationservice/FormationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cefomar.formationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationServiceApplication.class, args);
+	}
+
+}
---
+++ back/formation-service/src/test/java/com/cefomar/formationservice/FormationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefomar.formationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
